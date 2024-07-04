2e2ac9ee020fe865aad72b3624a91aec55aadf2d
MacBook <foysal11203004@gmail.com>
2024-07-04 17:25:15 +0600

    add controller, route, mehtod, view

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -6,6 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Welcome to my new project</h1>
+    <h1>Welcome to contact page</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/',[HomeController::class,'home']);
+
+Route::get('/contact-us',[ContactController::class,'contact']);
